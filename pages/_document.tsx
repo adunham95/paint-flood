dd0e3b6c9c714c93b07fc9c8d9552a85510030b1
Adrian <adunham95@gmail.com>
2023-08-04 22:45:38 -0400

    Added next eslint

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Paint Flood</title>
         <meta name="description" content="A paint flood game" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Paint Flood</title>
+      </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
